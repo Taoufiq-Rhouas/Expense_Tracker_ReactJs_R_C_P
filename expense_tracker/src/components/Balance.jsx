e8df5e0e82610d1395caaf0f77740b6c4335442d
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2023-10-03 21:13:33 +0100

    Styling_Balance_Component

--- expense_tracker/src/components/Balance.jsx
+++ expense_tracker/src/components/Balance.jsx
@@ -7,14 +7,13 @@ export default function Balance({balance}) {
         setCurrentBalance(balance)
     },[balance]);
 
-
     return (
-        <div className='bg-white border border-success text-center rounded'>
+        <div className={`bg-white border ${currentBalance >= 0 ? 'border-success' : 'border-danger'} text-center rounded`}>
             <p className="lead mt-3">
                 <span className="fw-bold me-2">
                     Balance:
                 </span>
-                <span className="badge bg-success">
+                <span className={`badge ${currentBalance >= 0 ? 'bg-success' : 'bg-danger'}`}>
                     $ {currentBalance}
                 </span>
             </p>
