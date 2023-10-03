f0ea57ce914c2dc6a9c7d50d306e0e57bc50a0eb
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2023-10-03 11:22:38 +0100

    Income_And_Expense_Component

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import Balance from "./components/Balance";
+import InExList from "./components/InExList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1, label: 'Rent', amount: '300'
+    },
+    {
+      id: 2, label: 'Phone repair', amount: '30'
+    }
+  ])
+
+  const [incomes, setIncomes] = useState([
+    {
+      id: 1, label: 'Salary', amount: '8000'
+    }
+  ])
   return (
     <div className="container">
       <div className="row my-4">
@@ -8,6 +24,7 @@ function App() {
           <div className="card">
             <div className="card-body" >
               <Balance balance="8000" />
+              <InExList incomes={incomes} expenses={expenses} />
             </div>
           </div>
         </div>
